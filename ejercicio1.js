6767c2ca771d0d5c900092c3f7a5a6ebb8c5beb5
juanPablo96 <juan.pablo1996@hotmail.com>
2023-05-18 15:42:19 -0300

    ejercicios 1 y 2

---
+++ ejercicio1.js
@@ -0,0 +1,33 @@
+////////////////// Ejercico 1///////////////////////
+
+/* PARTE 1 */
+
+/* let num1 = +prompt("elija un número por favor");
+let num2 = +prompt("elija otro número");
+
+alert(num1 + num2);
+
+console.log(typeof num1);
+
+
+
+ */
+
+/* PARTE 2 */
+/* let celciuis = +prompt("elija un una temperatura en grados celius");
+
+let fahrenheit = (celciuis * 9) / 5 + 32;
+alert(`eso equivale a ${fahrenheit} F°`); */
+/* PARTE 3 */
+
+/* 
+let num = +prompt("elija un número por favor");
+alert(num / 10); 
+*/
+
+/* PARTE 4 */
+
+/* 
+const a = [1, 2, 3];
+alert(typeof a);
+ */
---
+++ ejercicio2.js
@@ -0,0 +1,49 @@
+////////////////// Ejercico 2 ///////////////////////
+
+/* PARTE 1 */
+
+/*
+
+let num = +prompt("introduce un número");
+
+if (num % 2 == 0) {
+  alert("el número es par");
+} else {
+  alert("el número es impar");
+} */
+
+/* PARTE 2 */
+
+/*
+
+let num = +prompt("introduce un número");
+if (num % 5 == 0) {
+  alert(`${num} es divisible entre 5`);
+} else {
+  alert(`${num} no es divisible entre 5`);
+}
+console.log(num);
+ */
+
+/* PARTE 3 */
+
+/* let num = +prompt("introduce un número");
+if (num % 5 == 0 || num % 11 == 0) {
+  console.log(`${num} es divisible entre 5 o 11`);
+} else {
+  console.log(`${num} no es divisible entre 5 o 11`);
+} */
+
+/* PARTE 4 */
+
+/* let num1 = +prompt("introduce un número");
+let num2 = +prompt("introduce otro número");
+
+if (num1 > num2) {
+  alert(` el mayor número es: ${num1}`);
+} else {
+  alert(` el mayor número es: ${num2}`);
+}
+ */
+
+/* PARTE 5 */
